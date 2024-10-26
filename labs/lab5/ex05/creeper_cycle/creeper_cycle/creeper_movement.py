d89893089e5a25ae1893887d0cfafe904e6410ee
Porweks <svjatking42@mail.ru>
2024-10-26 20:04:02 +0700

    some fixes

--- labs/lab5/ex05/creeper_cycle/creeper_cycle/creeper_movement.py
+++ labs/lab5/ex05/creeper_cycle/creeper_cycle/creeper_movement.py
@@ -8,7 +8,7 @@ import math
 class MovementsNode(Node):
     def __init__(self):
         super().__init__('movements_node')
-        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/robot/cmd_vel', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.angle_step =  2 * math.pi / 1.5  # Угол для каждого лепестка
